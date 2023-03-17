2e21fc7aafbc25ee591c4e3208dfdcd316776b23
varnavinodv <116860438+varnavinodv@users.noreply.github.com>
2023-03-17 15:13:04 +0530

    Add files via upload

---
+++ student.sql
@@ -0,0 +1,20 @@
+create database details;
+use details;
+create table student (id  int,name varchar(10),address varchar(30),maths int(3),physics int(3),cs int(3),total int(4));
+select* from student;
+insert into student values(1,'adwaith','mavoor',98,90,92,280);
+insert into student values(2,'varna','kommeri',90,80,80,250);
+insert into student values(3,'rilu','eranjikkal',60,60,60,180);
+insert into student values(4,'saniga','nadakkav',50,50,52,150);
+insert into student values(5,'avinash','med clg',52,52,52,156);
+insert into student values(6,'niranjan','mavoor',98,90,92,280);
+insert into student values(7,'sree','kommeri',90,80,80,250);
+insert into student values(8,'abhinav','pottammal',60,60,60,180);
+insert into student values(9,'aswani','nadakkav',50,50,52,150);
+insert into student values(10,'ajay','med clg',52,52,52,156);
+insert into student values(11,'vijay','kozhikode',52,52,52,156);
+select * from student;
+select id,name from student;
+select id,name from student where total>250;
+select name from student where address='kozhikode';
+
